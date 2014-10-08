4ab97d5c0c8d5f0e38296cbc5010eabaa8f3ee75
Tomas Barton <barton.tomas@gmail.com>
2014-10-08 09:26:36 +0200

    simple memory usage reporting

---
+++ src/main/java/org/clueminer/report/MemInfo.java
@@ -0,0 +1,47 @@
+package org.clueminer.report;
+
+/**
+ * Class for measuring memory usage, it should be used only as a rough estimate
+ * of resources that is JVM allocating.
+ *
+ * @author Tomas Barton
+ */
+public class MemInfo {
+
+    private static final long MEGABYTE = 1024L * 1024L;
+    protected long startTime;
+    protected long startMemory;
+
+    public static long bytesToMegabytes(long bytes) {
+        return bytes / MEGABYTE;
+    }
+
+    public void startClock() {
+        // Get the Java runtime
+        Runtime runtime = Runtime.getRuntime();
+        // Run the garbage collector
+        runtime.gc();
+        startMemory = runtime.totalMemory() - runtime.freeMemory();
+        startTime = System.currentTimeMillis();
+
+    }
+
+    public void stopClock() {
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        System.out.println("time: " + elapsedTime / 1000 + "s, " + elapsedTime + "ms");
+    }
+
+    public void report() {
+        // Get the Java runtime
+        Runtime runtime = Runtime.getRuntime();
+        // Run the garbage collector
+        runtime.gc();
+        // Calculate the used memory
+        long memory = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Used memory: " + memory + " bytes");
+        System.out.println("Used memory: " + bytesToMegabytes(memory) + " MB");
+        System.out.println("Inc memory: " + bytesToMegabytes(memory - startMemory) + " MB");
+    }
+
+}
