4e2188473ff1f6017c4655690655acfafa542a04
Tomas Barton <barton.tomas@gmail.com>
2015-07-16 10:45:29 +0200

    fixtures for external criteria validation

---
+++ src/main/java/org/clueminer/fixtures/ClustFixture.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2011-2015 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.fixtures;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author deric
+ */
+public class ClustFixture extends AbstractFixture {
+
+    public File ext100p2() throws IOException {
+        return resource("clusterCrit/ext100_p2.csv");
+    }
+
+    public File ext100p3() throws IOException {
+        return resource("clusterCrit/ext100_p3.csv");
+    }
+
+}
