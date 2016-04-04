5b3f48e9b220931d8427bea6df2f0b8233073d8e
Tomas Barton <barton.tomas@gmail.com>
2016-04-04 11:41:56 +0200

    update copyright note

--- src/main/java/org/clueminer/fixtures/TimeseriesFixture.java
+++ src/main/java/org/clueminer/fixtures/TimeseriesFixture.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011-2016 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.clueminer.fixtures;
 
 import java.io.File;
@@ -38,7 +54,7 @@ public class TimeseriesFixture extends AbstractFixture {
     public File trace1() {
         return resource("ts/trace1.csv");
     }
-    
+
     public File ap01() {
         return resource("ts/AP-01.csv");
     }
