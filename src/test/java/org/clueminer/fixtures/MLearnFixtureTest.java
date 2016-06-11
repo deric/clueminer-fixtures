d38daabca804ee182de041597aeaec7db61192fd
Tomas Barton <barton.tomas@gmail.com>
2016-06-11 12:14:25 +0200

    correlations file

--- src/test/java/org/clueminer/fixtures/MLearnFixtureTest.java
+++ src/test/java/org/clueminer/fixtures/MLearnFixtureTest.java
@@ -1,9 +1,23 @@
+/*
+ * Copyright (C) 2011-2016 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.clueminer.fixtures;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  *
@@ -17,73 +31,41 @@ public class MLearnFixtureTest {
         subject = new MLearnFixture();
     }
 
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of iris method, of class MLearnFixture.
-     */
     @Test
     public void testIris() throws Exception {
         assertTrue(subject.iris().exists());
     }
 
-    /**
-     * Test of bosthouse method, of class MLearnFixture.
-     */
     @Test
     public void testBosthouse() throws Exception {
         assertTrue(subject.bosthouse().exists());
     }
 
-    /**
-     * Test of irisMissing method, of class MLearnFixture.
-     */
     @Test
     public void testIrisMissing() throws Exception {
         assertTrue(subject.irisMissing().exists());
     }
 
-    /**
-     * Test of irisQuoted1 method, of class MLearnFixture.
-     */
     @Test
     public void testIrisQuoted1() throws Exception {
         assertTrue(subject.irisQuoted1().exists());
     }
 
-    /**
-     * Test of irisQuoted2 method, of class MLearnFixture.
-     */
     @Test
     public void testIrisQuoted2() throws Exception {
         assertTrue(subject.irisQuoted2().exists());
     }
 
-    /**
-     * Test of cars method, of class MLearnFixture.
-     */
     @Test
     public void testCars() throws Exception {
         assertTrue(subject.cars().exists());
     }
 
-    /**
-     * Test of dermatology method, of class MLearnFixture.
-     */
     @Test
     public void testDermatology() throws Exception {
         assertTrue(subject.dermatology().exists());
     }
 
-    /**
-     * Test of forrestFires method, of class MLearnFixture.
-     */
     @Test
     public void testForrestFires() throws Exception {
         assertTrue(subject.forrestFires().exists());
@@ -92,5 +74,10 @@ public class MLearnFixtureTest {
     @Test
     public void testIrBaClassification() throws Exception {
         assertTrue(subject.irBaClassification().exists());
+    }
+
+    @Test
+    public void testCorrelation() throws Exception {
+        assertTrue(subject.correlations().exists());
     }
 }
