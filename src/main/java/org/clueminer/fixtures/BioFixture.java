538d7356357929f390e242113412ab198debda44
Tomas Barton <barton.tomas@gmail.com>
2016-06-16 09:23:33 +0200

    add fasta data

---
+++ src/main/java/org/clueminer/fixtures/BioFixture.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2011-2016 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.fixtures;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author Tomas Barton
+ */
+public class BioFixture extends AbstractFixture {
+
+    private static String folder = "bio/";
+
+    public BioFixture() {
+        super();
+    }
+
+    public File apData() throws IOException {
+        return resource(folder + "AP-01_2012112.arff");
+    }
+
+    public File genomeFasta() throws IOException {
+        return resource(folder + "Genome03EqSet01.fasta");
+    }
+}
---
+++ src/test/java/org/clueminer/fixtures/BioFixtureTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2011-2016 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.fixtures;
+
+import java.io.File;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author deric
+ */
+public class BioFixtureTest {
+
+    private static BioFixture instance;
+
+    public BioFixtureTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        instance = new BioFixture();
+    }
+
+    @Test
+    public void testTestData() throws Exception {
+        File result = instance.apData();
+        assertEquals(true, result.exists());
+
+    }
+
+    @Test
+    public void testGenomeFasta() throws Exception {
+        File result = instance.genomeFasta();
+        assertEquals(true, result.exists());
+
+    }
+}
