7d772c00385c99c0f892836ba18b63902faea8be
Tomas Barton <barton.tomas@gmail.com>
2016-11-14 11:43:23 +0100

    add network data

---
+++ src/test/java/org/clueminer/fixtures/NetworkFixtureTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2011-2016 clueminer.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.clueminer.fixtures;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author deric
+ */
+public class NetworkFixtureTest {
+
+    private static NetworkFixture instance;
+
+    @Before
+    public void setUp() {
+        instance = new NetworkFixture();
+    }
+
+    @Test
+    public void testKarate() throws Exception {
+        assertTrue(instance.karate().exists());
+    }
+
+}
